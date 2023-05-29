e3b983ae3e11256e93235351df271774bf481a7e
Nestor <nestorandrespenabautista@gmail.com>
2023-05-28 19:12:38 -0600

    Crud Semi Completo

---
+++ IDGS904_BD/Controllers/AlumnoController.cs
@@ -0,0 +1,78 @@
+﻿using IDGS904_BD.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace IDGS904_BD.Controllers
+{
+    public class AlumnoController : Controller
+    {
+        // GET: Alumno
+        public ActionResult Index()
+        {
+            using(AlumnosDBContext dbAlumnos = new AlumnosDBContext())
+            {
+                //select * from alumnos
+                return View(dbAlumnos.Alumnos.ToList());
+            }
+
+
+            return View();
+        }
+        public ActionResult Details( int? id)
+        {
+            using (AlumnosDBContext dbAlumnos = new AlumnosDBContext())
+            {
+                Alumnos alumnos = dbAlumnos.Alumnos.Find(id);
+                if(alumnos == null)
+                {
+                    return HttpNotFound();
+                }
+                return View(alumnos);
+            }
+        }
+        public ActionResult Edit(int? id)
+        {
+            using (AlumnosDBContext dbAlumnos = new AlumnosDBContext())
+            {
+                Alumnos alumnos = dbAlumnos.Alumnos.Find(id);
+                if (alumnos == null)
+                {
+                    return HttpNotFound();
+                }
+                return View(alumnos);
+            }
+        }
+        public ActionResult Edit(int? id,Alumnos alum)
+        {
+            using (AlumnosDBContext dbAlumnos = new AlumnosDBContext())
+            {
+                if (ModelState.IsValid)
+                {
+                    dbAlumnos.Entry(alum).State= EntityState.Modified;
+                    dbAlumnos.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+
+                return View(alum);
+            }
+        }
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(Alumnos alum)
+        {
+            using (AlumnosDBContext dbAlumnos = new AlumnosDBContext())
+            {
+               dbAlumnos.Alumnos.Add(alum);
+                dbAlumnos.SaveChanges();
+            }
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ IDGS904_BD/Models/AlumnosDBContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace IDGS904_BD.Models
+{
+    public class AlumnosDBContext:DbContext
+
+    {
+
+        private static string connection = "DefaultConnection";
+        public AlumnosDBContext() : base(connection) { }
+
+        public DbSet<Alumnos> Alumnos { get; set; }
+
+
+    }
+}
